01b47d95227fb7bbb8694bd8d82fda8261d66c11
David Langford <langfod@gmail.com>
2018-01-29 03:00:46 -0500

    formatting cleanup

--- src/test/java/virtualpet/VirtualPetTest.java
+++ src/test/java/virtualpet/VirtualPetTest.java
@@ -3,7 +3,6 @@ package virtualpet;
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class VirtualPetTest {
 
 	@Test
@@ -11,68 +10,70 @@ public class VirtualPetTest {
 		VirtualPet mypet = new VirtualPet();
 		Assert.assertEquals(mypet.getClass(), VirtualPet.class);
 	}
-	
+
 	@Test
 	public void VirtualPetgetHungerInitial() {
 		VirtualPet mypet = new VirtualPet();
 		Assert.assertEquals(20, mypet.getHunger());
 	}
-	
+
 	@Test
 	public void VirtualPetgetThirstInitial() {
 		VirtualPet mypet = new VirtualPet();
 		Assert.assertEquals(20, mypet.getThirst());
 	}
-	
+
 	@Test
 	public void VirtualPetgetHealthInitial() {
 		VirtualPet mypet = new VirtualPet();
 		Assert.assertEquals(90, mypet.getHealth());
 	}
-	
+
 	@Test
 	public void VirtualPetgetSleepinessInitial() {
 		VirtualPet mypet = new VirtualPet();
 		Assert.assertEquals(10, mypet.getSleepy());
 	}
-	
+
 	@Test
 	public void VirtualPetgetHappyInitial() {
 		VirtualPet mypet = new VirtualPet();
 		Assert.assertEquals(90, mypet.getHappy());
 	}
-	
+
 	@Test
 	public void VirtualPetgetPoopynessInitial() {
 		VirtualPet mypet = new VirtualPet();
 		Assert.assertEquals(20, mypet.getPoopy());
 	}
-	
+
 	@Test
 	public void VirtualPetgetNameInitial() {
 		VirtualPet mypet = new VirtualPet();
 		Assert.assertEquals("Bob", mypet.getName());
 	}
-	
+
 	@Test
 	public void VirtualPetgetDescritptionInitial() {
 		VirtualPet mypet = new VirtualPet();
 		Assert.assertEquals("Wombat", mypet.getDescription());
 	}
-	
-	@Test public void feedPetCheckNewValues() {
+
+	@Test
+	public void feedPetCheckNewValues() {
 		VirtualPet mypet = new VirtualPet();
 		mypet.feed();
 		Assert.assertEquals(5, mypet.getHunger());
-	
+
 		Assert.assertEquals(25, mypet.getThirst());
 		Assert.assertEquals(95, mypet.getHappy());
 		Assert.assertEquals(35, mypet.getPoopy());
 		Assert.assertEquals(95, mypet.getHealth());
 		Assert.assertEquals(20, mypet.getSleepy());
 	}
-	
-	@Test public void waterPetCheckNewValues() {
+
+	@Test
+	public void waterPetCheckNewValues() {
 
 		VirtualPet mypet = new VirtualPet();
 		mypet.water();
@@ -81,33 +82,36 @@ public class VirtualPetTest {
 		Assert.assertEquals(25, mypet.getPoopy());
 		Assert.assertEquals(91, mypet.getHealth());
 	}
-	
-	@Test public void walkPetCheckNewValues() {
+
+	@Test
+	public void walkPetCheckNewValues() {
 		VirtualPet mypet = new VirtualPet();
 		mypet.walk();
-		Assert.assertEquals(25, mypet.getHunger());	
+		Assert.assertEquals(25, mypet.getHunger());
 		Assert.assertEquals(25, mypet.getThirst());
 		Assert.assertEquals(100, mypet.getHappy());
 		Assert.assertEquals(0, mypet.getPoopy());
 		Assert.assertEquals(92, mypet.getHealth());
 		Assert.assertEquals(15, mypet.getSleepy());
 	}
-	
-	@Test public void sleepPetCheckNewValues() {
+
+	@Test
+	public void sleepPetCheckNewValues() {
 		VirtualPet mypet = new VirtualPet();
 		mypet.sleep();
-		Assert.assertEquals(40, mypet.getHunger());	
+		Assert.assertEquals(40, mypet.getHunger());
 		Assert.assertEquals(30, mypet.getThirst());
 		Assert.assertEquals(92, mypet.getHappy());
 		Assert.assertEquals(30, mypet.getPoopy());
 		Assert.assertEquals(92, mypet.getHealth());
 		Assert.assertEquals(0, mypet.getSleepy());
 	}
-	
-	@Test public void playPetCheckNewValues() {
+
+	@Test
+	public void playPetCheckNewValues() {
 		VirtualPet mypet = new VirtualPet();
 		mypet.play();
-		Assert.assertEquals(25, mypet.getHunger());	
+		Assert.assertEquals(25, mypet.getHunger());
 		Assert.assertEquals(30, mypet.getThirst());
 		Assert.assertEquals(100, mypet.getHappy());
 		Assert.assertEquals(91, mypet.getHealth());
